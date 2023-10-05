4e1f630508aef8566f742051d1d8fdb5047b5ee0
Gabriel Aron <arongabrielleal@gmail.com>
2023-10-05 16:31:26 -0300

    Criando o método para SELECT

--- src/main/java/br/com/carstore/servlet/model/Car.java
+++ src/main/java/br/com/carstore/servlet/model/Car.java
@@ -1,2 +1,13 @@
-package br.com.carstore.servlet.model;public class Car {
+package br.com.carstore.servlet.model;
+
+public class Car {
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
